caff763d5acf90ce67b6cdcd45c24246b6477f83
Nishit Jha <jhanishitback@gmail.com>
2022-10-12 14:11:57 +0530

    hi

--- app/modules/Dashboard/index.js
+++ app/modules/Dashboard/index.js
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import PrimaryButton from "../../components/Button/Primary";
-import styles from "./index.module.scss";
-import Textfield from "../../components/Textfield";
-import { useState } from "react";
-import axios from "../../utils/axios";
-import useSession from "../../utils/hooks/useSession";
-import classNames from "classnames/bind";
+import Image from 'next/image';
+import PrimaryButton from '../../components/Button/Primary';
+import styles from './index.module.scss';
+import Textfield from '../../components/Textfield';
+import { useState } from 'react';
+import axios from '../../utils/axios';
+import useSession from '../../utils/hooks/useSession';
+import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
-import PageStyles from "../../styles/page.module.scss";
-import TextField from "../../components/Textfield";
-import TextButton from "../../components/Button/Text";
-import Loader from "../../components/Button/Loader";
+import PageStyles from '../../styles/page.module.scss';
+import TextField from '../../components/Textfield';
+import TextButton from '../../components/Button/Text';
+import Loader from '../../components/Button/Loader';
 
 export default function Content() {
   const [tabIndex, setTabIndex] = useState(0);
@@ -60,17 +60,40 @@ export default function Content() {
                 </div>
               </div>
             </div>
+
+            <div className={styles.content__header__passengers}>Passengers</div>
           </div>
 
           <div className={styles.content__body}>
             <div className={styles.content__body__places}>
               <div className={styles.content__body__places__place}>
-                <TextField />
+                <TextField
+                  icon={
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        x="2"
+                        y="2"
+                        width="20"
+                        height="20"
+                        rx="10"
+                        stroke="#2E3742"
+                        stroke-width="4"
+                      />
+                    </svg>
+                  }
+                  placeholder="Departure City"
+                />
               </div>
               <div className={styles.content__body__places__circle}>
                 <div className={styles.content__body__places__circle__icon}>
                   <svg
-                    width="22"
+                    width="18"
                     height="18"
                     viewBox="0 0 22 18"
                     fill="none"
@@ -84,9 +107,26 @@ export default function Content() {
                 </div>
               </div>
               <div className={styles.content__body__places__place}>
-                <TextField placeholder={"Departure City"} />
+                <TextField
+                  placeholder={'Destination City'}
+                  icon={
+                    <svg
+                      width="18"
+                      height="24"
+                      viewBox="0 0 18 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9.00001 11.4166C8.22646 11.4166 7.4846 11.1094 6.93762 10.5624C6.39063 10.0154 6.08334 9.27353 6.08334 8.49998C6.08334 7.72643 6.39063 6.98457 6.93762 6.43759C7.4846 5.8906 8.22646 5.58331 9.00001 5.58331C9.77356 5.58331 10.5154 5.8906 11.0624 6.43759C11.6094 6.98457 11.9167 7.72643 11.9167 8.49998C11.9167 8.883 11.8412 9.26227 11.6947 9.61614C11.5481 9.97001 11.3332 10.2915 11.0624 10.5624C10.7916 10.8332 10.47 11.0481 10.1162 11.1946C9.76231 11.3412 9.38303 11.4166 9.00001 11.4166ZM9.00001 0.333313C6.83408 0.333313 4.75685 1.19373 3.2253 2.72527C1.69376 4.25682 0.833344 6.33405 0.833344 8.49998C0.833344 14.625 9.00001 23.6666 9.00001 23.6666C9.00001 23.6666 17.1667 14.625 17.1667 8.49998C17.1667 6.33405 16.3063 4.25682 14.7747 2.72527C13.2432 1.19373 11.1659 0.333313 9.00001 0.333313Z"
+                        fill="#2E3742"
+                      />
+                    </svg>
+                  }
+                />
               </div>
             </div>
+            <div className={styles.content__body__dates} />
           </div>
         </div>
       </div>
