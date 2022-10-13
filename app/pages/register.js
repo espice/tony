75eaafc4981739dd19002711d0a389ce60351668
Yash Tripathi <yashtri22@gmail.com>
2022-10-13 23:54:01 +0530

    sab bakwaas

--- app/pages/register.js
+++ app/pages/register.js
@@ -1,13 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import Header from "../components/Header";
+
 import Layout from "../components/Layout";
-import Content from "../modules/Register";
+
 
 export default function Home() {
   return (
     <Layout page={{ title: "Register" }}>
-      <Content />
+      {/* <Content /> */}
     </Layout>
   );
 }
