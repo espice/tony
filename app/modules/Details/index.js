9769c453bf0815dd88416096446548506a7d6af8
Yash Tripathi <yashtri22@gmail.com>
2022-10-13 20:36:49 +0530

    ok buddy retard

--- app/modules/Details/index.js
+++ app/modules/Details/index.js
@@ -2,6 +2,7 @@ import styles from "./index.module.scss";
 import PageStyles from "../../styles/page.module.scss";
 import Layout from "../../components/Layout";
 import Image from "next/image";
+import TextButton from "../../components/Button/Text";
 
 const Details = () => {
     return (
@@ -33,7 +34,7 @@ const Details = () => {
             <div className={styles.flights}>
                 <div className={styles.flight}>
                     <div className={styles.flight__image}>
-                        <Image src={"/logo.svg"} height={"30px"} width={"30px"}></Image>
+                        <Image style={{}} src={"/logo.svg"} height={"56px"} width={"56px"}></Image>
                     </div>
                     <div className={styles.flight__texts}>
                         <h3 className={styles.flight__texts__dates}>
@@ -43,7 +44,52 @@ const Details = () => {
                         Flight 7X49AE · Boeing 737 MAX
                         </h3>
                     </div>
-                    
+                    <div className={styles.flight__textss}>
+                    <h3 className={styles.flight__time}>Non Stop</h3>
+                    <h3 className={styles.flight__change}><TextButton>Change Flight</TextButton></h3>
+                    </div>
+                </div>
+                <div className={styles.flight} style={{borderTop: "2px solid #D4D4D4", borderTopLeftRadius: "0px", borderTopRightRadius: "0px"}}>
+                    <div className={styles.flight__image}>
+                        <Image style={{}} src={"/logo.svg"} height={"56px"} width={"56px"}></Image>
+                    </div>
+                    <div className={styles.flight__texts}>
+                        <h3 className={styles.flight__texts__dates}>
+                            Mon, Oct 25 2022 &nbsp; &nbsp; &nbsp; &nbsp; 05:00 to 07:15
+                        </h3>
+                        <h3 className={styles.flight__texts__flights}>
+                        Flight 7X49AE · Boeing 737 MAX
+                        </h3>
+                    </div>
+                    <div className={styles.flight__textss}>
+                    <h3 className={styles.flight__time}>Non Stop</h3>
+                    <h3 className={styles.flight__change}><TextButton>Change Flight</TextButton></h3>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.subheading}>
+                BILLING
+                <div className={styles.subheading__line}></div>
+            </div>
+            <div className={styles.flightdetails}>
+                <div className={styles.column}>
+                    <div className={styles.column__heading}>
+                        FLIGHT
+                    </div>
+                    <div className={styles.column__text}>
+                        7X49AC
+                    </div>
+                </div>
+
+                <div className={styles.column__right}>
+                    <div className={styles.column}>
+                        <div className={styles.column__heading}>
+                            PRICE
+                        </div>
+                        <div className={styles.column__text}>
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
