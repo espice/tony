8b6aa711cd0109f32379c2ceb16bc87e9d4fb20f
Yash Tripathi <yashtri22@gmail.com>
2022-10-13 23:50:33 +0530

    b

--- app/pages/profile.js
+++ app/pages/profile.js
@@ -1,13 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Header from '../components/Header';
+
 import Layout from '../components/Layout';
-import Content from '../modules/Profile';
 
 export default function Login() {
   return (
     <Layout page={{ title: 'Profile' }}>
-      <Content />
+      {/* <Content /> */}
     </Layout>
   );
 }
